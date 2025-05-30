4da09bcebbdc17ebb2748086ae8a55b4b0de0559
V๏ɪ፝֟d <97348572+VoidAryan@users.noreply.github.com>
2025-05-30 13:20:22 +0530

    Update Story.jsx
    updatedd

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -49,12 +49,12 @@ const FloatingImage = () => {
     <div id="story" className="min-h-dvh w-screen bg-black text-blue-50">
       <div className="flex size-full flex-col items-center py-10 pb-24">
         <p className="font-general text-sm uppercase md:text-[10px]">
-          the multiversal ip world
+          The multiverse of AI
         </p>
 
         <div className="relative size-full">
           <AnimatedTitle
-            title="the st<b>o</b>ry of <br /> a hidden real<b>m</b>"
+            title="The st<b>o</b>ry of <br /> the Nex<b>o</b>ra grid"
             containerClass="mt-5 pointer-events-none mix-blend-difference relative z-10"
           />
 
@@ -106,14 +106,14 @@ const FloatingImage = () => {
         <div className="-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end">
           <div className="flex h-full w-fit flex-col items-center md:items-start">
             <p className="mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start">
-              Where realms converge, lies Zentry and the boundless pillar.
-              Discover its secrets and shape your fate amidst infinite
-              opportunities.
+              Where intelligent layers converge, lies Nexora — the 
+              boundless AI pillar.
+              Unlock its secrets and shape your future through limitless possibilities.
             </p>
 
             <Button
               id="realm-btn"
-              title="discover prologue"
+              title="Chat with Us"
               containerClass="mt-5"
             />
           </div>
